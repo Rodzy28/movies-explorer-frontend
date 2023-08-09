8d1a745c2545973937691d407f2b3efcae03fa19
Rodzy28 <rodzy28@yandex.ru>
2023-08-09 18:36:01 +0300

    Доделал Регистрацию и Логин

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -11,38 +11,38 @@ export default function Login({ children }) {
         <main className='login'>
             <div className='login__container'>
 
-            <div className='login__header'>
-                <Link className='login__logo' to='/'>
-                    <img className='login__logo-size' src={logo} alt='Логотип' />
-                </Link>
-                {pathname === '/sign-in' ? <h1 className='login__title'>Рады видеть!</h1> : <h1 className='login__title'>Добро пожаловать!</h1>}
-            </div>
+                <div className='login__header'>
+                    <Link className='login__logo' to='/'>
+                        <img className='login__logo-size' src={logo} alt='Логотип' />
+                    </Link>
+                    {pathname === '/sign-in' ? <h1 className='login__title'>Рады видеть!</h1> : <h1 className='login__title'>Добро пожаловать!</h1>}
+                </div>
 
-            <form className='form'>
-                {children}
-                <label className='form__label'>
-                    <span className='form__label-title'>E-mail</span>
-                    <input className='form__input' type='email' name='email'
-                        placeholder='pochta@yandex.ru|' autoComplete='off' required />
-                </label>
-                <span className='form__input-error'>Что-то пошло не так...</span>
-                <label className='form__label'>
-                    <span className='form__label-title'>Пароль</span>
-                    <input className='form__input' type='password' name='password'
-                        placeholder='••••••••••••••' minLength='5' maxLength='15' autoComplete='off' required />
-                </label>
-                <span className='form__input-error'>Что-то пошло не так...</span>
-            </form>
+                <form className='form'>
+                    {children}
+                    <label className='form__label'>
+                        <span className='form__label-title'>E-mail</span>
+                        <input className='form__input' type='email' name='email'
+                            placeholder='pochta@yandex.ru|' autoComplete='off' required />
+                    </label>
+                    <span className='form__input-error'>Что-то пошло не так...</span>
+                    <label className='form__label'>
+                        <span className='form__label-title'>Пароль</span>
+                        <input className='form__input' type='password' name='password'
+                            placeholder='••••••••••••••' minLength='5' maxLength='15' autoComplete='off' required />
+                    </label>
+                    <span className='form__input-error'>Что-то пошло не так...</span>
+                </form>
 
-            <div className='login__button'>
-                <button type='submit'>
-                    {pathname === '/sign-in' ? 'Войти' : 'Зарегистрироваться'}
-                </button>
-                <p>
-                    {pathname === '/sign-in' ? 'Ещё не зарегистрированы?' : 'Уже зарегистрированы?'}
-                </p>
-                {pathname === '/sign-in' ? <Link to='/sign-up'>Регистрация</Link> : <Link to='/sign-in'>Войти</Link>}
-            </div>
+                <div className={`login__button ${pathname === '/sign-in' ? 'login__button_padding' : ''}`}>
+                    <button className='button_blue' type='submit'>
+                        {pathname === '/sign-in' ? 'Войти' : 'Зарегистрироваться'}
+                    </button>
+                    <span className='login__text'>
+                        {pathname === '/sign-in' ? 'Ещё не зарегистрированы?' : 'Уже зарегистрированы?'}
+                    </span>
+                    {pathname === '/sign-in' ? <Link className='login__link' to='/sign-up'>Регистрация</Link> : <Link className='login__link' to='/sign-in'>Войти</Link>}
+                </div>
             </div>
 
         </main>
