1c18d484302925e6cd0c9642d6157d3eacd11b01
Rodzy28 <rodzy28@yandex.ru>
2023-08-10 18:49:37 +0300

    Смена цвета шапки по Логину

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,9 @@ import logo from '../../images/logo.svg';
 import Navigation from '../Navigation/Navigation';
 
 export default function Header({ loggedIn }) {
+
   return (
-    <header className="header">
+    <header className={`header ${!loggedIn ? 'header_dark' : ''}`}>
       <div className='header__container'>
         <Link to='/' className='header__logo'>
           <img className='header__logo-size' src={logo} alt='Логотип' />
