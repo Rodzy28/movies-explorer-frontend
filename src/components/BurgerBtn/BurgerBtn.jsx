1f92c008e0820eb91e6396ceb586334f9eccc893
Rodzy28 <rodzy28@yandex.ru>
2023-08-12 20:06:11 +0300

    Очередной адаптив :/

--- src/components/BurgerBtn/BurgerBtn.jsx
+++ src/components/BurgerBtn/BurgerBtn.jsx
@@ -14,18 +14,20 @@ export default function BurgerBtn() {
             {menuOpen &&
                 <>
                     <button className='burger__close' type='button' onClick={showMenu} />
-                    <nav className='burger__menu'>
-                        <ul className='burger__links'>
-                            <li className='burger__item'><Link className='burger__link' to='/'>Главная</Link></li>
-                            <li className='burger__item'>
-                                <NavLink className={({ isActive }) => `burger__link ${isActive ? 'burger__link_active' : ''}`} to='/movies'>Фильмы</NavLink>
-                            </li>
-                            <li className='burger__item'>
-                                <NavLink className={({ isActive }) => `burger__link ${isActive ? 'burger__link_active' : ''}`} to='//saved-movies'>Сохранённые фильмы</NavLink>
-                            </li>
-                        </ul>
-                        <ProfileBtn />
-                    </nav>
+                    <div className='burger__cover'>
+                        <nav className='burger__menu'>
+                            <ul className='burger__links'>
+                                <li className='burger__item'><Link className='burger__link' to='/'>Главная</Link></li>
+                                <li className='burger__item'>
+                                    <NavLink className={({ isActive }) => `burger__link ${isActive ? 'burger__link_active' : ''}`} to='/movies'>Фильмы</NavLink>
+                                </li>
+                                <li className='burger__item'>
+                                    <NavLink className={({ isActive }) => `burger__link ${isActive ? 'burger__link_active' : ''}`} to='/saved-movies'>Сохранённые фильмы</NavLink>
+                                </li>
+                            </ul>
+                            <ProfileBtn />
+                        </nav>
+                    </div>
                 </>
             }
         </>
