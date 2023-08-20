90209df3d1fc24a6e4689ea206039a09256cad7d
Sergei Pradko <rodzy28@yandex.ru>
2023-08-21 00:13:02 +0300

    Доделать цвета в Header

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,28 @@
 import './Navigation.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export default function Navigation() {
-    return (
-        <nav className='navigation'>
-            <ul className='navigation__list'>
-                <li className='navigation__item'><NavLink className={({isActive}) => `navigation__link ${isActive ? 'navigation__link_active' : ''}`} to='/movies'>Фильмы</NavLink></li>
-                <li className='navigation__item'><NavLink className={({isActive}) => `navigation__link ${isActive ? 'navigation__link_active' : ''}`} to='/saved-movies'>Сохранённые фильмы</NavLink></li>
-            </ul>
-        </nav>
-    );
+
+  const { pathname } = useLocation();
+
+  return (
+    <nav className='navigation'>
+      <ul className='navigation__list'>
+        <li className='navigation__item'>
+          <NavLink className={({ isActive }) => `navigation__link
+          ${pathname === '/' ? 'navigation__link_color_white' : ''}
+          ${isActive ? 'navigation__link_active' : ''}`} to='/movies'>
+            Фильмы
+          </NavLink>
+        </li>
+        <li className='navigation__item'>
+          <NavLink className={({ isActive }) => `navigation__link
+          ${pathname === '/' ? 'navigation__link_color_white' : ''}
+          ${isActive ? 'navigation__link_active' : ''}`} to='/saved-movies'>
+            Сохранённые фильмы
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
